08ad2ea6af2bffa4fb747a78ae03092e34a1e84f
quyennguyen <nguyenquyen0201qt@gmail.com>
2020-03-14 15:29:13 +0700

    my project sucessfull

---
+++ shoppingbackend/src/main/java/com/nqt/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.nqt.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
