69955a04807021bd69667189cb47febd344574f6
quyennguyen <nguyenquyen0201qt@gmail.com>
2020-03-15 14:00:03 +0700

    Load static resources

--- minishopping/src/main/java/com/nqt/minishopping/controller/PageController.java
+++ minishopping/src/main/java/com/nqt/minishopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.nqt.minishopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,5 +14,23 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting","Welcome to spring mvc");
 		return mv;
+	}
+	/*
+	 * @RequestMapping(value = "/test") public ModelAndView test(@RequestParam(value
+	 * = "greeting", required = false) String greeting) { if (greeting == null) {
+	 * greeting = "Hello world"; } ModelAndView mv = new ModelAndView("page");
+	 * mv.addObject("", greeting); return mv;
+	 * 
+	 * }
+	 */
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String  greeting) {
+		if (greeting == null) {
+			greeting = "Hello world";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("", greeting);
+		return mv;
+		
 	}
 }
